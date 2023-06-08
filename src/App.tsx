ad0db2324e5990042eb2c03f26a87f0684169008
karabass92 <shatveryank@gmail.com>
2023-06-08 22:00:32 +0600

    creat navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Fragment } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import FavouritesPage from './pages/FavouritesPage';
+import Navigation from './components/Navigation';
 
 
 function App() {
     return (
-        <div className="App">
-            hello
-        </div>
+        <Fragment>
+            <Navigation />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/favourites' element={<FavouritesPage />} />
+            </Routes>
+        </Fragment>
     );
-}
+};
 
 
 export default App;
